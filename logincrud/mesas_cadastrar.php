5fed6262fd88ffdeae9ff8d2cd6420f9eaeefb58
henrique0202 <henrique.silva101@aluno.ce.gov.br>
2025-06-25 21:48:11 -0300

    Update mesas_cadastrar.php

--- logincrud/mesas_cadastrar.php
+++ logincrud/mesas_cadastrar.php
@@ -20,10 +20,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<h2>Cadastrar Nova Mesa</h2>
-<form method="post">
-    Número: <input type="number" name="numero" required><br><br>
-    Capacidade: <input type="number" name="capacidade" required><br><br>
-    Disponível? <input type="checkbox" name="disponibilidade" checked><br><br>
-    <button type="submit">Salvar</button>
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Mesa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Cadastrar Nova Mesa</h2>
+        <form method="post">
+            <label>Número:</label><br>
+            <input type="number" name="numero" required><br><br>
+
+            <label>Capacidade:</label><br>
+            <input type="number" name="capacidade" required><br><br>
+
+            <label><input type="checkbox" name="disponibilidade" checked> Disponível</label><br><br>
+
+            <button type="submit">Salvar</button>
+        </form>
+        <br><a href="mesas_listar.php">← Voltar para lista</a>
+    </div>
+</body>
+</html>
